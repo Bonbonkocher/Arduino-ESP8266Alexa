26056c6fd20fc00046b4fe57347d033c24418e47
Bonbonkocher <johnnydepp1987@hotmail.de>
2021-04-12 21:56:17 +0200

    Garage Auf Zu

---
+++ Arduino-ESP8266Alexa_Garage/WifiConnect.h
@@ -0,0 +1,8 @@
+/************
+*Die die daten von deinen Router
+* Beispiel:
+*#define WIFI_SSID "Fritzbox 7490"
+*#define WIFI_PASS "1234567890"
+************/
+#define WIFI_SSID "WifiName"
+#define WIFI_PASS "DeinPasswort"
